f1e80fe320667d8ce45d029db38c6ea8e23a0849
Aaron Zachariah <aaron.zachariah@gmail.com>
2024-04-14 19:27:44 -0400

    added poisoned cora graphs from laptop

--- CLGA/dep.cmd
+++ CLGA/dep.cmd
@@ -1,8 +1,9 @@
+REM gensim uses scipy functionality that was removed in scipy versions >= 1.13 so we need to install an older version
+pip install scipy==1.12
 pip install torch
 pip install torch_geometric
-pip install pyyaml
-pip install nni
-pip install torch-scatter
 pip install ogb
 pip install numba
 pip install gensim
+pip install pyyaml
+pip install nni
