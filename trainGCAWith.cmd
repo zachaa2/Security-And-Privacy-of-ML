5497384cd64f3ac13da638696f78d94f476ed12b
Aaron Zachariah <aaron.zachariah@gmail.com>
2024-04-17 02:33:13 -0400

    for cga experiments

--- trainGCAWith.cmd
+++ trainGCAWith.cmd
@@ -2,38 +2,46 @@
 
 REM Check args
 IF "%~1"=="" (
-    echo Usage: %0 method
+    echo Usage: %0 method1 method2 ...
     exit /b 1
 )
-REM set method from args
-SET METHOD=%1
 
-REM rates
+REM Define rates
 SET RATES=0.01 0.05 0.10
-REM allowed methods
-SET ALLOWED_METHODS="metattack random dice minmax pgd nodeembeddingattack"
 
-REM Check if arg method is allowed
-echo %ALLOWED_METHODS% | findstr /R /C:"\<%METHOD%\>" >nul 2>&1
-IF ERRORLEVEL 1 (
-    echo Invalid method. Allowed methods are: %ALLOWED_METHODS%
-    exit /b 1
-)
+REM Define allowed methods
+SET ALLOWED_METHODS="dice minmax pgd"
 
-REM Change to correct directory
+REM Change to the correct directory
 cd .\CLGA\
 
 REM Activate virtual environment
 call venv\Scripts\activate
 
-REM train gca with given method for each rate
+REM Loop through each argument provided
+:nextmethod
+SHIFT
+IF "%~1"=="" GOTO endmethods
+SET METHOD=%1
+
+REM Check if the current arg method is allowed
+echo %ALLOWED_METHODS% | findstr /R /C:"\<%METHOD%\>" >nul 2>&1
+IF ERRORLEVEL 1 (
+    echo Invalid method: %METHOD%. Allowed methods are: %ALLOWED_METHODS%
+    goto nextmethod
+)
+
+REM Train GCA with the given method for each rate
 FOR %%R IN (%RATES%) DO (
     echo Training GCA with %METHOD% and rate %%R
-    REM tain gca
-    python -u .\train_GCA.py --dataset Cora --perturb --attack_method %METHOD% --attack_rate %%R --device cpu
+    REM Train GCA
+    python -u .\train_GCA.py --dataset Cora --perturb --attack_method %METHOD% --attack_rate %%R --device cuda:0
     echo Done with method %METHOD% and rate %%R
 )
 
+GOTO nextmethod
+
+:endmethods
 REM Deactivate virtual environment
 call deactivate
 
